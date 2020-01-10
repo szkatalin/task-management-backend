83ea5015947916ae2389ca5b9357b9b62d228696
Szalai Katalin <szalai.katalin@autsoft.hu>
2020-01-10 16:11:45 +0100

    add Auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, AuthModule],
   controllers: [],
   providers: [],
 })
