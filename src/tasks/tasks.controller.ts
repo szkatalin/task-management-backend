46196f84091f0c75bf8d18ab7f008bbaf40bf640
Szalai Katalin <szalai.katalin@autsoft.hu>
2020-01-10 13:40:14 +0100

    get tasks by filtering them in url params

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,18 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import {Task, TaskStatus} from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import {GetTasksFilterDto} from './dto/get-tasks-filter.dto';
 
 @Controller('tasks') // which route should be handled
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    }
     return this.tasksService.getAllTasks();
   }
 
